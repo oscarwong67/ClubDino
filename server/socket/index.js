639e4120396c0e2a65f39988ca5fe350f323b9b3
Jeremy Olea <jeremy.olea18@gmail.com>
2022-02-19 16:28:09 -0700

    socket io init

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,5 +1,5 @@
 const gameRooms = {
-  // [roomKey]: {
+  // [roomId]: {
   // users: [],
   // randomTasks: [],
   // scores: [],
@@ -7,6 +7,12 @@ const gameRooms = {
   // players: {},
   // numPlayers: 0
   // }
+
+  tfdl: {
+    players: {},
+    numPlayers: 0,
+    roomId: 'tfdl',
+  }
 };
 
 module.exports = (io) => {
@@ -19,5 +25,83 @@ module.exports = (io) => {
       ? socket.emit("keyIsValid", input)
       : socket.emit("keyNotValid");
     });
+
+    socket.on("joinRoom", (roomId) => {
+      socket.join(roomId);
+      const roomInfo = gameRooms[roomId];
+      // console.log("roomInfo", roomInfo);
+      roomInfo.players[socket.id] = {
+        rotation: 0,
+        x: 400,
+        y: 300,
+        playerId: socket.id,
+      };
+
+      // update number of players
+      roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+
+      // set initial state
+      socket.emit("setState", roomInfo);
+
+      // send the players object to the new player
+      socket.emit("currentPlayers", {
+        players: roomInfo.players,
+        numPlayers: roomInfo.numPlayers,
+        roomId: roomInfo.roomId,
+      });
+
+      // update all other players of the new player
+      socket.to(roomId).emit("newPlayer", {
+        playerInfo: roomInfo.players[socket.id],
+        numPlayers: roomInfo.numPlayers,
+        roomId: roomInfo.roomId,
+      });
+    });
+
+    // when a player moves, update the player data
+    socket.on("playerMovement", function (data) {
+      const { x, y, roomId } = data;
+      // console.log(gameRooms[roomId].players);
+      // console.log(socket.id);
+      gameRooms[roomId].players[socket.id].x = x;
+      gameRooms[roomId].players[socket.id].y = y;
+      // emit a message to all players about the player that moved
+      socket
+        .to(roomId)
+        .emit("playerMoved", gameRooms[roomId].players[socket.id]);
+    });
+
+    // when a player disconnects, remove them from our players object
+    socket.on("disconnect", function () {
+      console.log('disconnecting player');
+      //find which room they belong to
+      let roomId = 0;
+      for (let keys1 in gameRooms) {
+        for (let keys2 in gameRooms[keys1]) {
+          Object.keys(gameRooms[keys1][keys2]).map((el) => {
+            if (el === socket.id) {
+              roomId = keys1;
+            }
+          });
+        }
+      }
+
+      const roomInfo = gameRooms[roomId];
+
+      if (roomInfo) {
+        console.log("user disconnected: ", socket.id);
+        // remove this player from our players object
+        delete roomInfo.players[socket.id];
+        // update numPlayers
+        roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+        // emit a message to all players to remove this player
+        io.to(roomId).emit("disconnected", {
+          playerId: socket.id,
+          numPlayers: roomInfo.numPlayers,
+          roomId: roomInfo.roomId,
+        });
+      }
+    });
+
   });
 };
--- src/scenes/tfdl.js
+++ src/scenes/tfdl.js
@@ -3,15 +3,160 @@ import Phaser from "phaser";
 export default class TFDL extends Phaser.Scene {
   constructor() {
     super("TFDL");
+    this.state = {};
   }  
+
+  preload() {
+    this.load.spritesheet("astronaut", "assets/spritesheets/astronaut3.png", {
+      frameWidth: 29,
+      frameHeight: 37,
+    });
+  }
 
   init() {
     // socket
     this.socket = io();
+  }  
+
+  addPlayer(scene, playerInfo) {
+    scene.joined = true;
+    scene.astronaut = scene.physics.add
+      .sprite(playerInfo.x, playerInfo.y, "astronaut")
+      .setOrigin(0.5, 0.5)
+      .setSize(30, 40)
+      .setOffset(0, 24);
+  }
+
+  addOtherPlayers(scene, playerInfo) {
+    const otherPlayer = scene.add.sprite(
+      playerInfo.x,
+      playerInfo.y,
+      "astronaut"
+    );
+    otherPlayer.playerId = playerInfo.playerId;
+    scene.otherPlayers.add(otherPlayer);
   }
 
   create() {
     const text = this.add.text(400, 250, 'Hello World!');
     text.setOrigin(0, 0);
+
+    const scene = this;
+    
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // CREATE OTHER PLAYERS GROUP
+    this.otherPlayers = this.physics.add.group();
+
+    this.socket.on("newPlayer", function (arg) {
+      const { playerInfo, numPlayers, roomId } = arg;
+      scene.addOtherPlayers(scene, playerInfo);
+      scene.state.numPlayers = numPlayers;
+
+      console.log(scene.state.players);
+    });
+
+    // JOINED ROOM - SET STATE
+    this.socket.on("setState", function (state) {
+      const { roomId, players, numPlayers } = state;
+      scene.physics.resume();
+
+      // STATE
+      scene.state.roomId = roomId;
+      scene.state.players = players;
+      scene.state.numPlayers = numPlayers;
+
+      console.log(scene.state);
+    });
+
+    // PLAYERS
+    this.socket.on("currentPlayers", function (arg) {
+      const { players, numPlayers } = arg;
+      scene.state.numPlayers = numPlayers;
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === scene.socket.id) {
+          scene.addPlayer(scene, players[id]);
+        } else {
+          scene.addOtherPlayers(scene, players[id]);
+        }
+      });
+    });
+
+    this.socket.on("playerMoved", function (playerInfo) {
+      scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerInfo.playerId === otherPlayer.playerId) {
+          const oldX = otherPlayer.x;
+          const oldY = otherPlayer.y;
+          otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+        }
+      });
+    });
+
+    this.socket.on("otherPlayerStopped", function (playerInfo) {
+      this.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerInfo.playerId === otherPlayer.playerId) {
+          otherPlayer.anims.stop(null, true);
+        }
+      });
+    });
+
+    // DISCONNECT
+    this.socket.on("disconnected", function (arg) {
+      const { playerId, numPlayers } = arg;
+      scene.state.numPlayers = numPlayers;
+      scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerId === otherPlayer.playerId) {
+          otherPlayer.destroy();
+        }
+      });
+    });    
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    scene.socket.emit("joinRoom", "tfdl");
+  }  
+
+  update() {
+    const scene = this;
+    if (scene.astronaut) {
+      const speed = 225;
+      // Stop any previous movement from the last frame
+      scene.astronaut.body.setVelocity(0);
+      // Horizontal movement
+      if (scene.cursors.left.isDown) {
+        scene.astronaut.body.setVelocityX(-speed);
+      } else if (scene.cursors.right.isDown) {
+        scene.astronaut.body.setVelocityX(speed);
+      }
+      // Vertical movement
+      if (scene.cursors.up.isDown) {
+        scene.astronaut.body.setVelocityY(-speed);
+      } else if (scene.cursors.down.isDown) {
+        scene.astronaut.body.setVelocityY(speed);
+      }
+      // Normalize and scale the velocity so that astronaut can't move faster along a diagonal
+      this.astronaut.body.velocity.normalize().scale(speed);
+
+      // emit player movement
+      var x = scene.astronaut.x;
+      var y = scene.astronaut.y;
+      if (
+        scene.astronaut.oldPosition &&
+        (x !== scene.astronaut.oldPosition.x ||
+          y !== scene.astronaut.oldPosition.y)
+      ) {
+        scene.moving = true;
+        scene.socket.emit("playerMovement", {
+          x: scene.astronaut.x,
+          y: scene.astronaut.y,
+          roomId: scene.state.roomId,
+        });
+      }
+      // save old position data
+      scene.astronaut.oldPosition = {
+        x: scene.astronaut.x,
+        y: scene.astronaut.y,
+        rotation: scene.astronaut.rotation,
+      };
+    }
   }
 }
