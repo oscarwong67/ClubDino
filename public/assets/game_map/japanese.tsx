833509dcd3df9a2930a1a8b076b6f7dcabc24254
Oscar Wong <wong.oscar67@gmail.com>
2022-02-20 13:59:38 -0700

    tsx

--- public/assets/game_map/japanese.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="japanese" tilewidth="32" tileheight="32" tilecount="512" columns="16">
- <image source="20_Japanese_interiors_32x32.png" width="512" height="1024"/>
-</tileset>
